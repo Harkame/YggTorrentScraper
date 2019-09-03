9da8ebd0a4968a5264be1b8352713bb95e74b459
Harkame <louisr.daviaud@gmail.com>
2019-09-03 13:42:18 +0200

    define some tests

---
+++ tests/test_connection.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import unittest
+
+import requests
+from yggtorrentscraper import YggTorrentScraper
+
+my_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, my_path + '/../japscandownloader/')
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+
+
+class TestConnection(unittest.TestCase):
+    scraper = YggTorrentScraper(requests.session())
+
+    def test_login(self):
+        self.scraper.login('myidentifiant', 'mypassword')
+
+    def test_logout(self):
+        self.scraper.logout()
