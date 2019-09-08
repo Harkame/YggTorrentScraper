6a3de828793199cddc5e512a655a3b57f12741a2
Daviaud Louis <louis-richard.daviaud@etu.umontpellier.fr>
2019-09-08 23:35:23 +0200

    prepare more test search

--- yggtorrentscraper/tests/test_search.py
+++ yggtorrentscraper/tests/test_search.py
@@ -10,11 +10,30 @@ class TestResearch(unittest.TestCase):
 
     def test_search(self):
         torrents_url = self.scraper.search(name="walking dead s08", category="", sub_category="",
-                                           descriptions=None, files=None, uploaders=None, sort="publish_date", order="asc")
+                                           descriptions=None, files='mkv', uploaders={'Team_QTZ'}, sort="publish_date", order="asc")
 
-        torrent = self.scraper.extract_details(torrent_url=torrents_url[0])
+        #torrent = self.scraper.extract_details(torrent_url=torrents_url[0])
+    '''
+    def test_search_name(self):
+        torrents_url = self.scraper.search(name="walking dead")
+
+    def test_search_category(self):
+        torrents_url = self.scraper.search(name="walking dead")
+
+    def test_search_sub_category(self):
+        torrents_url = self.scraper.search(name="walking dead")
+
+    def test_search_descriptions(self):
+        torrents_url = self.scraper.search(name="walking dead")
+
+    def test_search_uploaders(self):
+        torrents_url = self.scraper.search(name="walking dead")
+
+    def test_search_sort(self):
+        torrents_url = self.scraper.search(name="walking dead")
+    '''
 
     def test_search_multiple_page(self):
         torrents_url = self.scraper.search(name="walking dead")
 
-        torrent = self.scraper.extract_details(torrent_url=torrents_url[0])
+        self.assertTrue(len(torrents_url) > 200)
