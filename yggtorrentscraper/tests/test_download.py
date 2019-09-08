ab088c7db3fe76c059ade65b6eadded81e78493e
Daviaud Louis <louis-richard.daviaud@etu.umontpellier.fr>
2019-09-08 11:51:40 +0200

    test search multiple tab

--- yggtorrentscraper/tests/test_download.py
+++ yggtorrentscraper/tests/test_download.py
@@ -28,23 +28,15 @@ class TestDownload(unittest.TestCase):
         if not os.path.exists(self.destination_path):
             os.makedirs(self.destination_path)
 
-    def test_download_success(self):
+    def test_download(self):
         most_completed = self.scraper.most_completed()
 
         torrent = self.scraper.extract_details(most_completed[0])
 
         self.assertTrue(torrent.url is not None)
 
-        self.scraper.download_from_torrent(torrent, self.destination_path)
-
-    def test_download_failed(self):
-        most_completed = self.scraper.most_completed()
-
-        torrent = self.scraper.extract_details(most_completed[0])
-
-        self.assertTrue(torrent.url is not None)
-
-        # TODO
+        self.scraper.download_from_torrent(
+            torrent=torrent, destination_path=self.destination_path)
 
     def tearDown(self):
         if os.path.exists(self.destination_path):
--- yggtorrentscraper/tests/test_search.py
+++ yggtorrentscraper/tests/test_search.py
@@ -8,8 +8,13 @@ from ..yggtorrentscraper import YggTorrentScraper
 class TestResearch(unittest.TestCase):
     scraper = YggTorrentScraper(requests.session())
 
-    def test_research(self):
+    def test_search(self):
         torrents_url = self.scraper.search(name="walking dead s08", category="", sub_category="",
                                            descriptions=None, files=None, uploaders=None, sort="publish_date", order="asc")
+
+        torrent = self.scraper.extract_details(torrent_url=torrents_url[0])
+
+    def test_search_multiple_page(self):
+        torrents_url = self.scraper.search(name="walking dead")
 
         torrent = self.scraper.extract_details(torrent_url=torrents_url[0])
