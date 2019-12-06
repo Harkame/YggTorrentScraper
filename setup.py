fa18efa7db39a1c38edfc86baebf979573ccd270
Harkame <louisr.daviaud@gmail.com>
2019-12-06 13:36:30 +0100

    1.2.0

--- setup.py
+++ setup.py
@@ -5,17 +5,13 @@ with open("README.md", "r") as fh:
 
 setup(
     name="yggtorrentscraper",
-    version="1.1.10",
+    version="1.2.0",
     author="Harkame",
     description="Scraper for YggTorrent",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Harkame/YggTorrentDownloader",
-    install_requires = [
-        "BeautifulSoup4 >= 4.8.1",
-        "lxml >= 4.4.1",
-        "requests >= 2.22.0"
-    ],
+    install_requires=["BeautifulSoup4 >= 4.8.1", "lxml >= 4.4.1", "requests >= 2.22.0"],
     classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved",
